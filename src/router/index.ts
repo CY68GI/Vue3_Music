95cce95c16838b35ed7222137876ccc5a46c53ff
CY68GI <caohongyu386@gmail.com>
2025-08-09 19:54:16 +0800

    添加注释和格式化代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router' /** * 导入Vue Router相关功能 * createRouter: 用于创建路由实例 * createWebHistory: 用于创建HTML5历史模式的路由 */
 
-const router = createRouter({
+const router = createRouter({ /** * 创建路由实例 * history: 使用HTML5历史模式，基于环境变量中的BASE_URL * routes: 初始化一个空数组，表示当前没有定义任何路由规则 */
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [],
 })
 
-export default router
+export default router //  导出路由实例，供其他组件使用
